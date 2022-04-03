65d5436cc4135bdaa8e4ac364aa24698a4327769
datata <danitt15@gmail.com>
2022-04-03 22:17:49 +0200

    feat: me endpoint user

--- the_final_count_days_app/app/Http/Controllers/AuthController.php
+++ the_final_count_days_app/app/Http/Controllers/AuthController.php
@@ -53,4 +53,9 @@ class AuthController extends Controller
 
         return response()->json(compact('token'));
     }
+
+    public function me(): JsonResponse
+    {
+        return response()->json(auth()->user());;
+    }
 }
--- the_final_count_days_app/routes/api.php
+++ the_final_count_days_app/routes/api.php
@@ -19,6 +19,12 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::group([
+    'middleware' => 'jwt.auth',
+], function () {
+    Route::get('/me', [AuthController::class, 'me']);
+});
 
